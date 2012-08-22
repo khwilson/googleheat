1d4ad07888e7684291d81067780110bbed05dcba
Kevin <khwilson@gmail.com>
2012-08-21 23:36:39 -0400

    Made pull.py a function instead of a main

--- pull.py
+++ pull.py
@@ -21,29 +21,59 @@ longmu = -84.484863
 latsigma = 3.5
 longsigma = 3.5
 
-for query in range(maxquery):
-    url = 'http://maps.googleapis.com/maps/api/distancematrix/json?origins=%3.4f,%3.4f&destinations=' % (latmu, longmu)
-    destinations = [ '%3.4f,%3.4f' % (gauss(latmu, latsigma), gauss(longmu, longsigma)) for dummy in range(numdests) ]
-    url += '|'.join( destinations ) 
-    url += '&units=metric&sensor=false'
-    req = urllib2.Request(url)
-    google_out = urllib2.urlopen(req).read()
-
+def make_query(origins, destinations):
+   origins_str = map(str, origins)
+   destinations_str = map(str, destinations)
+   url = ('http://maps.googleapis.com/maps/api/distancematrix/json?' +
+          'origins=' + '|'.join(origins_str) +
+          '&destinations=' + '|'.join(destinations_str))
+   url += '|'.join( destinations ) 
+   url += '&units=metric&sensor=false'
+   req = urllib2.Request(url)
+   google_out = urllib2.urlopen(req).read()
+   
     # See if we're over the temporary limit
-    if google_out.find('OVER') != -1:
-       time.sleep(10)
-       google_out = urllib2.urlopen(req).read()
-       if google_out.find('OVER') != -1:
-          print "Apparently you're over the daily limit for requests"
-          break
-    
-    # If it's a good request, write it out
-    filename = 'output%d' % (int(time.time() * 1000))
-    f = open(filename, 'wb')
-    jOut = json.loads(google_out)
-    jOut['latlongs'] = destinations
-    f.write(json.dumps(jOut))
+   if google_out.find('OVER') != -1:
+      time.sleep(10)
+      google_out = urllib2.urlopen(req).read()
+      if google_out.find('OVER') != -1:
+         raise ValueError("Over the daily limit for requests")
+   
+   return google_out
 
+
+if __name__=='__main__':
+   r"""
+   Add back in a test guy
+   """
+
+   maxquery = 25
+   numdests = 50
+
+   # witchita:
+   latmu = 38.08269
+   longmu = -84.484863
+   # roughly 400 miles:
+   latsigma = 3.5
+   longsigma = 3.5
+
+   for query in range(maxquery):
+      destinations = [ '%3.4f,%3.4f' % 
+                       (gauss(latmu, latsigma), gauss(longmu, longsigma)) 
+                       for dummy in range(numdests) ]
+      try:
+         google_out = make_query([ ','.join([str(latmu), str(longmu)])], 
+                                 destinations)
+      except ValueError, e:
+         print e
+         exit()
+
+      # If it's a good request, write it out
+      filename = 'output%d' % (int(time.time() * 1000))
+      f = open(filename, 'wb')
+      jOut = json.loads(google_out)
+      jOut['latlongs'] = destinations
+      f.write(json.dumps(jOut))
 
 
 
